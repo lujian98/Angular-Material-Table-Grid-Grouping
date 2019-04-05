1c73c9d97dbb2c4a9b943307bc2b1d73deb38ad4
lujian98 <lujian98@yahoo.com>
2019-04-05 12:53:48 -0700

    simplify group model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { MatTableDataSource } from '@angular/material';
 import { People, people  } from './sample-data';
 
 export class Group {
-  level: number = 0;
+  level = 0;
   parent: Group;
-  expanded: boolean = true;
+  expanded = true;
+  totalCounts = 0;
   get visible(): boolean {
     return !this.parent || (this.parent.visible && this.parent.expanded);
   }
@@ -20,13 +21,11 @@ export class Group {
 export class AppComponent {
   title = 'Grid Grouping';
 
-
-
   displayedColumns: string[] = ['surname', 'forename', 'gender', 'ukCity', 'salary', 'department'];
   public dataSource = new MatTableDataSource<People | Group>([]);
 
-  groupByColumns: string[] = ['department', 'salary'];
-
+  // groupByColumns: string[] = ['department', 'salary'];
+  groupByColumns: string[] = ['department'];
   columns: any[];
   constructor() {
     this.columns = [{
@@ -54,71 +53,77 @@ export class AppComponent {
   getDataRowVisible(data: People): boolean {
     const groupRows = this.dataSource.data.filter(
       row => {
-        if (!(row instanceof Group)) return false;
-
+        if (!(row instanceof Group)) {
+          return false;
+        }
         let match = true;
-        this.groupByColumns.forEach(
-          column => {
-            if (!row[column] || !data[column] || row[column] !== data[column]) match = false;
+        this.groupByColumns.forEach(column => {
+          if (!row[column] || !data[column] || row[column] !== data[column]) {
+            match = false;
           }
-        );
+        });
         return match;
       }
     );
 
-    if (groupRows.length === 0) return true;
-    if (groupRows.length > 1) throw "Data row is in more than one group!";
-    const parent = <Group>groupRows[0];  // </Group> (Fix syntax coloring)
+    if (groupRows.length === 0) {
+      return true;
+    }
+    // if (groupRows.length > 1) {
+    //   throw "Data row is in more than one group!";
+    // }
 
+    const parent = groupRows[0] as Group;
     return parent.visible && parent.expanded;
   }
 
   groupHeaderClick(row) {
-    row.expanded = !row.expanded
+    row.expanded = !row.expanded;
     this.dataSource.filter = performance.now().toString();  // hack to trigger filter refresh
   }
 
   addGroups(data: any[], groupByColumns: string[]): any[] {
-    var rootGroup = new Group();
+    const rootGroup = new Group();
     return this.getSublevel(data, 0, groupByColumns, rootGroup);
   }
 
   getSublevel(data: any[], level: number, groupByColumns: string[], parent: Group): any[] {
     // Recursive function, stop when there are no more levels.
-    if (level >= groupByColumns.length)
+    if (level >= groupByColumns.length) {
       return data;
-
-    var groups = this.uniqueBy(
+    }
+    const groups = this.uniqueBy(
       data.map(
         row => {
-          var result = new Group();
+          const result = new Group();
           result.level = level + 1;
           result.parent = parent;
-          for (var i = 0; i <= level; i++)
+          for (let i = 0; i <= level; i++) {
             result[groupByColumns[i]] = row[groupByColumns[i]];
+          }
           return result;
         }
       ),
       JSON.stringify);
 
     const currentColumn = groupByColumns[level];
-
-    var subGroups = [];
+    let subGroups = [];
     groups.forEach(group => {
-      let rowsInGroup = data.filter(row => group[currentColumn] === row[currentColumn])
-      let subGroup = this.getSublevel(rowsInGroup, level + 1, groupByColumns, group);
+      const rowsInGroup = data.filter(row => group[currentColumn] === row[currentColumn]);
+      group.totalCounts = rowsInGroup.length;
+      const subGroup = this.getSublevel(rowsInGroup, level + 1, groupByColumns, group);
       subGroup.unshift(group);
       subGroups = subGroups.concat(subGroup);
-    })
+    });
     return subGroups;
   }
 
   uniqueBy(a, key) {
-    var seen = {};
-    return a.filter(function (item) {
-      var k = key(item);
+    const seen = {};
+    return a.filter((item) => {
+      const k = key(item);
       return seen.hasOwnProperty(k) ? false : (seen[k] = true);
-    })
+    });
   }
 
   isGroup(index, item): boolean {
