2f9c2c85977e4b5d559e680a7c521751285f0dd2
lujian98 <lujian98@yahoo.com>
2019-04-05 13:09:52 -0700

    simple data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 
-import { People, people  } from './sample-data';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 12, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 15, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 16, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 17, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 18, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 19, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 export class Group {
   level = 0;
@@ -21,30 +49,25 @@ export class Group {
 export class AppComponent {
   title = 'Grid Grouping';
 
-  displayedColumns: string[] = ['surname', 'forename', 'gender', 'ukCity', 'salary', 'department'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
   public dataSource = new MatTableDataSource<any | Group>([]);
 
   // groupByColumns: string[] = ['department', 'salary'];
-  groupByColumns: string[] = ['department'];
+  groupByColumns: string[] = ['symbol'];
   columns: any[];
   constructor() {
-    this.columns = [{
-      field: 'surname',
-    }, {
-      field: 'forename',
-    }, {
-      field: 'gender',
-    }, {
-      field: 'ukCity',
-    }, {
-      field: 'salary',
-    }, {
-      field: 'department',
-    }];
-
-    this.dataSource.data = this.addGroups(people, this.groupByColumns);
+    this.columns = [
+      { field: 'position', width: 100,  },
+      { field: 'name', width: 350, },
+      { field: 'weight', width: 250, },
+      { field: 'symbol', width: 100, }
+    ];
+    const data = ELEMENT_DATA;
+    this.dataSource.data = this.addGroups(data, this.groupByColumns);
     this.dataSource.filterPredicate = this.customFilterPredicate.bind(this);
   }
+
 
   customFilterPredicate(data: any | Group, filter: string): boolean {
     return (data instanceof Group) ? data.visible : this.getDataRowVisible(data);
